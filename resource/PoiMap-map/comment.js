e2c8d2fea6bf5b89df9610373bf28f23982a4976
masaya2019 <bjmk1290313@gn.iwasaki.ac.jp>
2019-10-05 17:56:58 +0900

    いろいろ追加しました

---
+++ resource/PoiMap-map/comment.js
@@ -0,0 +1,44 @@
+const info = document.getElementById('informaion');
+
+const comments = [
+  { comment: '球にあふれてる時がある。本当にこういうのはなしにしてほしい', created_at: '2019/04/01' },
+  {comment: 'いやあ、ほんとマジであり得ない。このごみ箱に、半分以上飲みかけのタピオカミルクティが入ってた。そのせいで手がびしょびしょ……ほんと買うのなら飲み切ってほしい', created_at: '2019 / 04 / 25' },
+  { comment: '瓶・缶・ペット・燃えるゴミとりあえず行けばなんでも捨てれるから横浜ついたらとりあえず捨てに行ってるかな？', created_at: '2019/05/04' },
+  { comment: '夜はゴミがあふれてる……', created_at: '2019/05/12' },
+  { comment: 'この場所にはタカラが眠ってる。これだからゴミあさりはやめられねえ。', created_at: '2019/05/15' },
+  { comment: 'うわあ、気持ち悪……', created_at: '2019/05/15'},
+  { comment: 'よいゴミ箱です!', created_at: '2019/08/12' },
+  { comment: 'よいゴミ箱です!', created_at: '2019/08/15' },
+  { comment: 'ダメなゴミ箱です!', created_at: '2019/08/17' },
+  { comment: 'よいゴミ箱です!', created_at: '2019/08/18' },
+  { comment: 'よいゴミ箱です!', created_at: '2019/08/19' },
+];
+
+const commentBox = document.createElement('div');
+commentBox.className = 'comment-box';
+
+for (let i = 0; i < comments.length; i++) {
+  const comment = document.createElement('div');
+  comment.className = 'comment';
+
+  const boxRight = document.createElement('div');
+  boxRight.className = 'comment-box-right';
+
+  const users = document.createElement('div');
+  users.className = 'users-icon';
+
+  const newComment = document.createElement('p');
+  newComment.className = 'comment-text';
+  newComment.innerText = comments[i].comment;
+
+  const newCommentTime = document.createElement('p');
+  newCommentTime.className = 'comment-time';
+  newCommentTime.innerHTML = comments[i].created_at;
+
+  info.appendChild(commentBox);
+  commentBox.appendChild(comment);
+  comment.appendChild(users);
+  comment.appendChild(boxRight);
+  boxRight.appendChild(newComment);
+  boxRight.appendChild(newCommentTime);
+}
---
+++ resource/PoiMap-map/googlemap.js
@@ -0,0 +1,103 @@
+const presentLocationData = {
+  lat: 35.465843,
+  lon: 139.622669
+};
+
+const destinationLocateData = {
+  lat: 35.4690564,
+  lon: 139.6211406
+};
+
+//現在地を中心に地図を表示する
+const initMap = () => {
+  const directionsService = new google.maps.DirectionsService();
+  const directionsRenderer = new google.maps.DirectionsRenderer();
+  const MyLatLng = new google.maps.LatLng(
+    presentLocationData['lat'],
+    presentLocationData['lon']
+  );
+  const mapOptions = {
+    zoom: 15,
+    center: MyLatLng,
+    mapTypeId: 'roadmap',
+    mapTypeControl: false,
+    fullscreenControl: false,
+    streetViewControl: false,
+    zoomControlOptions: {
+      position: google.maps.ControlPosition.LEFT_BOTTOM
+    }
+  };
+  //地図を表示する
+  const map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  directionsRenderer.setMap(map);
+  map.addListener('click', e => {
+    //マーカーを設置して、
+    getClickLatLng(e.latLng, map);
+    //let addBox = document.getElementById('addBox');
+  });
+  /*
+  //2地点間のルートを表示
+  const request = {
+    //現在地
+    origin: new google.maps.LatLng(
+      presentLocationData['lat'],
+      presentLocationData['lon']
+    ),
+    //目的地
+    destination: new google.maps.LatLng(
+      destinationLocateData['lat'],
+      destinationLocateData['lon']
+    ),
+    travelMode: google.maps.DirectionsTravelMode.WALKING
+  };
+  directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+      directionsRenderer.setDirections(result);
+    }
+  });
+  */
+};
+
+document.getElementById('return').addEventListener('click', () => {
+  window.location.href = '../PoiMap-home/PoiMap-home.html';
+});
+
+const getClickLatLng = (lat_lng, map) => {
+  // マーカーを設置
+  const marker = new google.maps.Marker({
+    position: lat_lng,
+    map: map
+  });
+  //
+  const hasSelectClass = $('#addBox').hasClass('addBox');
+  if (hasSelectClass) {
+    //あるとき
+    $('#addBox').removeClass('addBox');
+    $('#addBox').addClass('selectAddBox');
+    $('#addBoxText').append('新しくゴミ箱を追加しますか？');
+    $('#goAddForm').removeClass('boxButton');
+    $('#goAddForm').addClass('addBoxButton');
+    $('#goAddForm').html('はい');
+    $('#returnMap').removeClass('boxButton');
+    $('#returnMap').addClass('addBoxButton');
+    $('#returnMap').html('いいえ');
+  }
+  //消去
+  $('#returnMap').click(function() {
+    $('#addBox').removeClass('selectAddBox');
+    $('#addBox').addClass('addBox');
+    $('#addBoxText').html('');
+    $('#goAddForm').removeClass('addBoxButton');
+    $('#goAddForm').addClass('boxButton');
+    $('#goAddForm').html('');
+    $('#returnMap').removeClass('addBoxButton');
+    $('#returnMap').addClass('boxButton');
+    $('#returnMap').html('');
+    marker.setMap(null);
+  });
+  $('#goAddForm').click(function() {
+    //位置情報保存
+    console.log(lat_lng);
+    //投稿フォームへ
+  });
+};
