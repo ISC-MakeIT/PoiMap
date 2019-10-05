f8f4dca8e6891b24d9958119d6059f35dde154f9
masaya2019 <bjmk1290313@gn.iwasaki.ac.jp>
2019-10-06 00:14:19 +0900

    resetボタンでSVGの色を戻すように変更

--- resource/PoiMap-postScreen/PoiMap-postScreen.js
+++ resource/PoiMap-postScreen/PoiMap-postScreen.js
@@ -1,4 +1,3 @@
-//localStorage.clear();
 /*----------
   写真選択
 ----------*/
@@ -13,7 +12,7 @@ document.getElementById('uploadImage').onchange = function() {
     if (sessionStorage.getItem('upImage') === null) {
       sessionStorage.setItem('upImage', reader.result);
     } else {
-      sessionStorage.clear();
+      sessionStorage.removeItem('upImage');
       sessionStorage.setItem('upImage', reader.result);
     }
     //エラーが表示されていたら消す
@@ -27,23 +26,27 @@ document.getElementById('uploadImage').onchange = function() {
 /*-----------------
   ゴミ箱の種類選択
 -----------------*/
-// ゴミ箱の種類タッチしたらsvgの色を変更
+//ゴミ箱の種類タッチしたらsvgの色を変更
 $('.category').on('click', function() {
   const thisId = this.id;
-  if (this.id == 'petBottle') {
+  makeCapitalLetter(thisId);
+});
+
+const makeCapitalLetter = thisId => {
+  if (thisId == 'petBottle') {
     const capitalLetter = 'PetBottle';
     changeSVG(thisId, capitalLetter);
-  } else if (this.id == 'burnable') {
+  } else if (thisId == 'burnable') {
     const capitalLetter = 'Burnable';
     changeSVG(thisId, capitalLetter);
-  } else if (this.id == 'nonburnable') {
+  } else if (thisId == 'nonburnable') {
     const capitalLetter = 'NonBurnable';
     changeSVG(thisId, capitalLetter);
-  } else if (this.id == 'aluminumGlass') {
+  } else if (thisId == 'aluminumGlass') {
     const capitalLetter = 'AluminumGlass';
     changeSVG(thisId, capitalLetter);
   }
-});
+};
 
 const changeSVG = (thisId, capitalLetter) => {
   const hasNonSelectClass = $(`#${thisId}-icon`).hasClass(
@@ -91,7 +94,7 @@ const changeSVG = (thisId, capitalLetter) => {
   戻ったときの処理
 -----------------*/
 onload = function() {
-  /* 戻ったときにチェックされているもののSVGの色を変える */
+  //戻ったときにチェックされているもののSVGの色を変える
   $('input:checked').each(function() {
     const selectValue = $(this).val();
     if (selectValue == 'petBottle') {
@@ -108,7 +111,7 @@ onload = function() {
       changeSVG(selectValue, capitalLetter);
     }
   });
-  /* 戻ったときに写真を表示 */
+  //戻ったときに写真を表示
   if (!(sessionStorage.getItem('upImage') === null)) {
     const returnImage = sessionStorage.getItem('upImage');
     $('#preview').css(
@@ -157,3 +160,62 @@ $('form').validate({
     }
   }
 });
+
+/*------------------------
+  リセット押したときの処理
+------------------------*/
+$('#resetForm').click(function() {
+  //写真を消す
+  if (sessionStorage.getItem('upImage') === null) {
+    console.log('null');
+  } else {
+    sessionStorage.removeItem('upImage');
+    $('#preview').css('background', '');
+  }
+  const iconType = ['petBottle', 'burnable', 'nonburnable', 'aluminumGlass'];
+  for (i = 0; i < iconType.length; i++) {
+    const thisId = iconType[i];
+    console.log(thisId);
+    if (thisId == 'petBottle') {
+      const capitalLetter = 'PetBottle';
+      resetSvg(thisId, capitalLetter);
+    } else if (thisId == 'burnable') {
+      const capitalLetter = 'Burnable';
+      resetSvg(thisId, capitalLetter);
+    } else if (thisId == 'nonburnable') {
+      const capitalLetter = 'NonBurnable';
+      resetSvg(thisId, capitalLetter);
+    } else if (thisId == 'aluminumGlass') {
+      const capitalLetter = 'AluminumGlass';
+      resetSvg(thisId, capitalLetter);
+    }
+  }
+});
+
+const resetSvg = (thisId, capitalLetter) => {
+  const hasNonSelectClass = $(`#${thisId}-icon`).hasClass(
+    `nonSelect${capitalLetter}-icon`
+  );
+  if (hasNonSelectClass) {
+    //nonSelectの時
+  } else {
+    //selectの時
+    $(`#${thisId}-icon`).removeClass(`select${capitalLetter}-icon`);
+    $(`#${thisId}-icon`).addClass(`nonSelect${capitalLetter}-icon`);
+
+    if (thisId == 'nonburnable') {
+      $(`#nonSelect${capitalLetter}Icon`).removeClass(
+        `select${capitalLetter}Icon`
+      );
+      $(`#nonSelect${capitalLetter}Icon`).addClass(
+        `nonSelect${capitalLetter}Icon`
+      );
+      $(`#${thisId}-iconBorder`).css({
+        'background-color': '#c5cdd1',
+        border: '#c5cdd1 1px solid'
+      });
+    } else {
+      $(`#${thisId}-iconBorder`).css('border', '#c5cdd1 1px solid');
+    }
+  }
+};
